43e8e11866de19ca6282e22a67b787b09b33214b
KevinYao <Kydeepmind@gmail.com>
2026-05-18 23:21:17 -0700

    add Google Calendar callable token exchange

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,5 +2,4 @@ import * as admin from 'firebase-admin';
 
 admin.initializeApp();
 
-// Google Calendar sync functions will be exported here in subsequent increments.
-export {};
+export { exchangeGoogleAuthCode, unlinkGoogleCalendar } from './google-oauth';
